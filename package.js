46b019d36323fd7c94429f570e5b66dac14b6a4b
Gigacross <hinas418@gmail.com>
2015-01-12 19:35:47 +0800

    modified package.js for Meteor.

--- package.js
+++ package.js
@@ -1,5 +1,8 @@
 Package.describe({
-  summary: ""
+	name: 'meteor-bootstrap-confirmation',
+  summary: "Bootstrap powered popover confirmation.",
+  version: '1.0.0',
+  git: "https://github.com/Gigacross/meteor-bootstrap-confirmation.git"
 });
 
 Package.on_use(function (api, where) {
@@ -8,3 +11,4 @@ Package.on_use(function (api, where) {
 
   api.add_files('lib/bootstrap3_confirmation.js', 'client');
 });
+
