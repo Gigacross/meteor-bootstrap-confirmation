e92dacf4dd0b1827d50b45f7ec99080a87d16381
Gigacross <hinas418@gmail.com>
2015-01-13 00:34:36 +0800

    Package published successfully

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
-	name: 'gigacross:meteor-bootstrap-confirmation',
+	name: 'hinas:meteor-bootstrap-confirmation',
   summary: "Bootstrap powered popover confirmation.",
-  version: '1.0.0',
+  version: '0.9.0',
   git: "https://github.com/Gigacross/meteor-bootstrap-confirmation.git"
 });
 
 Package.on_use(function (api, where) {
   api.use('jquery', 'client');
-  api.imply('jquery', 'client');
+  // api.imply('jquery', 'client');
+
+  //version constraint
+  api.versionsFrom("0.9.4");
 
   api.add_files('lib/bootstrap3_confirmation.js', 'client');
 });
